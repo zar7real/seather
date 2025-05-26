1c4de42cee484ce850f6cb34113db1eb4e7b8cd7
alchemy <96349532+zar7real@users.noreply.github.com>
2025-05-26 17:46:20 +0200

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,5 @@
+import time as t
+print("Under construction...")
+t.sleep(1.3)
+print("this little launcher will help you coordinate the various attacks you want to do without you typing the commands. (it will be completely customizable as you want")
+t.sleep(100)
